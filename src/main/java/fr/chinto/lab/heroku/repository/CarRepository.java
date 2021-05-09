b857b156cd0a127a3dcd732d6b682e74042119b7
chinjto <contact@cyril-defaye.fr>
2021-05-10 01:11:44 +0200

    Tuto addons : Heroku Postgresql
    Erreur de typage des repo (inversion clé/valeur)

--- src/main/java/fr/chinto/lab/heroku/repository/CarRepository.java
+++ src/main/java/fr/chinto/lab/heroku/repository/CarRepository.java
@@ -3,4 +3,4 @@ package fr.chinto.lab.heroku.repository;
 import fr.chinto.lab.heroku.entity.Car;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CarRepository extends CrudRepository<Long, Car> { }
+public interface CarRepository extends CrudRepository<Car, Long> { }
--- src/main/java/fr/chinto/lab/heroku/repository/OwnerRepository.java
+++ src/main/java/fr/chinto/lab/heroku/repository/OwnerRepository.java
@@ -3,4 +3,4 @@ package fr.chinto.lab.heroku.repository;
 import fr.chinto.lab.heroku.entity.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Long, Owner> { }
+public interface OwnerRepository extends CrudRepository<Owner, Long> { }
