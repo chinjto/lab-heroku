e1bb1e21902000959b05f6171cb92f1b8f0c52a3
chinjto <contact@cyril-defaye.fr>
2021-05-09 22:33:56 +0200

    Initial commit

---
+++ src/test/java/fr/chinto/lab/heroku/LabHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.chinto.lab.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
